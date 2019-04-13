bc742d2d7d9a8a3ce2c1202d84eaf2acc1296f6f
kellidragon <dragonkelli@gmail.com>
2019-04-13 02:17:52 -0700

    changed game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,61 +5,41 @@ $(document).ready(function(){
 
 //start game 
 $(".start").on('click', startGame);
+$(".submit").on('click', gameOver);
+$("form").hide();
+$(".submit").hide();
 
-//hide option buttons before game start
-$(".option").hide();
 //variables to track 
 var wins= 0;
 var losses = 0;
-var unanswered =0;
 var clockRunning = false;
-var time = 30;
-var userAnswer =0;
-var currentQ =0;
-
-//  Variable that will hold setInterval that runs the stopwatch
-var intervalId;
-
-var trivia = [{
-    question:"Who is Britney Spears?", 
-    answers: [ "Madonna","Angelina Jolie","Britney Spears","Queen Latifa"],
-    correctAnswer: "Britney Spears"
-    },
-    {
-    question: "Who is Charlie Sheen?",
-    answers: ["Charlie Sheen","Donald Trump","William Shatner","Joaqin Pheonix"],
-    correctAnswer: "Charlie Sheen"
-}
-    // "Who is Bynes?": "Amanda Bynes", 
-    // "Who is Lilo?": "Lindsay Lohan",
-    // "Who is KW": "Kanye West",
-    // "Who is Joaquin Phoenix?": "Joaquin Phoenix",
-    // "Who is Snooki?": "Snooki",
-
+var time = 120;
+var answers= [
+"Britney Spears",
+"Charlie Sheen",
+"Snooki",
+"Lindsay Lohan",
+"Amanda Bynes",
+"Kanye West"
 ]
-console.log(trivia[0]);
-
-
-//show one question until player answers or time runs out
-
-
 
 
 //<----------------------------------------------------TIMER------------------------------------------------->
-
+//  Variable that will hold setInterval that runs the stopwatch
+var intervalId;
 
 function startGame() {
-   
 // setInterval to start the count here and set the clock to running.
 if (!clockRunning) {
     //to keep timer from speeding up
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
     clockRunning = true;
-    askQuestion();
-    gameLogic();
-    $(".option").show()
+    game();
+    $("form").show();
     $(".start").hide()
+    $("#descrip").hide();
+    $(".submit").show();
 }
 }
 
@@ -68,93 +48,79 @@ function decrement() {
     $("#timer-display").html("Time Remaining: " + time);
     if (time === 0) {
         stop();
+        gameOver();
 }
 }
-
-
-
-function askQuestion(){
-    for (var i = 0; i < trivia.length; i++) {
-    $('#question').text(trivia[i].question);
-    console.log(  $('#question').text())
-    $('.option1').text(trivia[i].answers[0]);
-    $('.option2').text(trivia[i].answers[1]);
-    $('.option3').text(trivia[i].answers[2]);
-    $('.option4').text(trivia[i].answers[3]);
-    console.log( $('.option1').text())
-
-}
-
-}
-
-
-function gameLogic(){
-    //on click event for answers
-//if user guesses correct answer
-$('.option').on("click", function(){
-    // event.preventDefault();
-    // console.log(event.target);
-   userChoice = $(this).text();
-   console.log(userChoice);
-   for (var i = 0; i < trivia.length; i++) {
-    if(userChoice === trivia[i].correctAnswer){
-       
-        $(".answer").text("Correct! The answer is: " + trivia[i].correctAnswer)
-        $(".image").html("<iframe src='https://media.giphy.com/media/JltOMwYmi0VrO/giphy.gif'>")
-        wins++
-        
-    //if user does not answer and time runs out 
-    } else if(this === -1){
-        $(".answer").text("Out of Time! The answer was: " + trivia[i].correctAnswer)
-        $(".image").html("<iframe src='https://media.giphy.com/media/zcVOyJBHYZvX2/giphy.gif'>")
-        unanswered++
-    } else{
-        $(".answer").text("Negative. The answer was: " + trivia[i].correctAnswer)
-        $(".image").html("<iframe src='https://media.giphy.com/media/zcVOyJBHYZvX2/giphy.gif'>")
-        losses++
-    }
-}
-    });
-    
-};  
-
-
-  
- 
-
 function stop() {
   
     // clearInterval to stop the count here and set the clock to not be running.
     clearInterval(intervalId);
     clockRunning = false;
+    //display results
 }
 
-//runs the start game function 
+
+function game(){
+
+    //on click event for answers
+//if user guesses correct answer
+
+    var formInput = $(".answer");
+    var checked = false;
+    var userAnswer;
+    
+    for( let i = 0; i < formInput.length; i++ ) {
+       if(formInput[i].checked) {
+         checked = true;
+         userAnswer = formInput[i].value;
+       }
+    } 
+    // if user clicks submit without filling out the form
+    if(!checked) {
+      return;
+    }
+    if(time===0){
+        gameOver();
+    }
+    // Correct answer
+
+    if(userAnswer === answers[i] && time > 0) {
+        wins++
+ 
+      ;
+    }
+    // incorrect answer
+    else {
+       lossess++
+
+    }
+    
+    }
 
 
 
-function restartGame() {
 
+function gameOver() {
+    clearInterval(intervalId);
+    clockRunning = false;
     time = 0;
-    lap = 1;
-  
-    // DONE: Change the "display" div to "00:00."
-    $("#display").text("00:00");
-  
-    // DONE: Empty the "laps" div.
-    $("#laps").text("");
-  }
-//player can only select one answer
-//if correct answer selected load new screen with congrats - wait a few seconds and then next question
-//if correct wrong selected load new screen with wrong and display correct answer - wait a few seconds and then next question
-//if time runs out load new screen - times up! wait a few seconds then load the next question
+    $("form").hide();
+    $(".submit").hide();
+    $("#descrip").hide();
+    $("#results").text("The Results Are In!")
+    $("#wins").text("Correct: "+ wins)
+    $("#losses").html("Wrong: "+ losses)
+    if(wins > 4){
+        $("#blurb").text("Looks like you know crazy pretty well..."+ '<br>')
+        $("#image").append("<iframe src='https://media.giphy.com/media/zcVOyJBHYZvX2/giphy.gif'>")
+    }else{
+        $("#blurb").text("Time to brush up on your crazy...")
+        $("#image").append("<iframe src='https://media.giphy.com/media/MyDiXB4PnPSBW/giphy.gif'>")
+}
 
-
-
-//after last question show screen with number of correct answers, wrong answers, button to restart game
-
-
+}
 })
+
 
 
 
